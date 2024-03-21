3471271ff9d8c85a6e0f11fcc6b9e70f11c6bef7
이종경 <gyoung3063413@naver.com>
2024-03-21 16:19:27 +0900

    Ex: 입력된 정수의 짝수와 홀수 판별 프로그램

---
+++ C/4.제어문/1.짝수홀수판별프로그램.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    int a;
+
+    printf("정수를 입력하시오: ");
+    scanf("%d", &a);
+
+    if (a % 2 == 0)
+        printf("%d는 짝수입니다.\n", a);
+    if (a % 2 == 1)
+        printf("%d는 홀수입니다.\n", a);
+}
